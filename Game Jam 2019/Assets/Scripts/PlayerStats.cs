b9e7365d133925ec31b006a1a9266e04d02432b8
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2019-07-01 15:07:09 +0200

    PlayerStats

---
+++ Game Jam 2019/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float health;
+    public float stamina;
+    public bool canAttack;
+
+    private void Awake()
+    {
+        canAttack = true;
+    }
+
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+    }
+}
