45511eb6091ca00016ccb720dc5f1ac7c7043a27
luuk boersma <49709526+Kluunkert@users.noreply.github.com>
2019-07-01 15:34:50 +0200

    Health and Stamina UI elements
    -63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63-63

--- Game Jam 2019/Assets/Scenes/UI Assets/UI Scripts/UIScript.cs
+++ Game Jam 2019/Assets/Scenes/UI Assets/UI Scripts/UIScript.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 
 public class UIScript : MonoBehaviour
 {
+    public GameObject panel;
+
+    public int paused = 0, unpause = 1;
+    public bool pause;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (pause == false)
+            {
+                PauseMenu();
+            } else
+            {
+                UnPaused();
+            }
+        }
+    }
     public void Play()
     {
         SceneManager.LoadScene(1);
@@ -23,5 +41,24 @@ public class UIScript : MonoBehaviour
     public void back()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void Continue()
+    {
+        panel.SetActive(false);
+    }
+
+    public void PauseMenu()
+    {
+        panel.SetActive(true);
+        Time.timeScale = paused;
+        pause = true;
+    }
+
+    public void UnPaused()
+    {
+        panel.SetActive(false);
+        Time.timeScale = unpause;
+        pause = false;
     }
 }
