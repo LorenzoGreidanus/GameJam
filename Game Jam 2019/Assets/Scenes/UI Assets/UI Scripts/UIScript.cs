eee635574afab89aea448c9bf87adfb22fd5403f
luuk boersma <49709526+Kluunkert@users.noreply.github.com>
2019-07-01 11:28:01 +0200

    Main Menu :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: v
    Main Menu UI :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire: :100: :fire:

---
+++ Game Jam 2019/Assets/Scenes/UI Assets/UI Scripts/UIScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
