34087f1febbf5252178b1190b4563dbc835c8841
luuk boersma <49709526+Kluunkert@users.noreply.github.com>
2019-07-02 09:48:46 +0200

    New shit
    Shits new yo

---
+++ Game Jam 2019/Assets/Scenes/UI Assets/UI Scripts/Sound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0, 1)]
+    public float volume;
+    [Range(0.1f, 3)]
+    public float pitch;
+}
